19a46ff8640dc117a48aa2770dda38cdde0cbd8c
Михаил Савенко <mi47sav@gmail.com>
2025-01-08 10:58:42 +0300

    Patch запрос отменен для Dog И Breed ViewSet

--- dog_api/api/views.py
+++ dog_api/api/views.py
@@ -4,11 +4,13 @@ from breed.models import Breed
 from api.serializers import DogSerializer, BreedSerializer
 
 
-class DogViewSet(viewsets.ModelViewSet):
+class DogViewSet(viewsets.ModelViewSet): 
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
+    http_method_names = ["get", "post", "put", "delete"]
 
 
-class BreedViewSet(viewsets.ModelViewSet):
+class BreedViewSet(viewsets.ModelViewSet): 
     queryset = Breed.objects.all()
     serializer_class = BreedSerializer
+    http_method_names = ["get", "post", "put", "delete"]
