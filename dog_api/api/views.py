80ffa115810014dfd2cb6880b873df8bc49cef02
Михаил Савенко <mi47sav@gmail.com>
2025-01-08 13:15:53 +0300

    Переопределен get_queryset У DogViewSet. Используются  методы с аннотацией дополнительных полей

--- dog_api/api/views.py
+++ dog_api/api/views.py
@@ -5,9 +5,19 @@ from api.serializers import DogSerializer, BreedSerializer
 
 
 class DogViewSet(viewsets.ModelViewSet): 
-    queryset = Dog.objects.all()
     serializer_class = DogSerializer
     http_method_names = ["get", "post", "put", "delete"]
+
+    def get_queryset(self):
+        queryset = Dog.objects.select_related("breed")
+
+        if self.action == "list":
+            return queryset.with_breed_average_age()
+        
+        elif self.action == 'retrieve':
+            return queryset.with_same_breed_count()
+        
+        return queryset
 
 
 class BreedViewSet(viewsets.ModelViewSet): 
--- dog_api/dog/managers.py
+++ dog_api/dog/managers.py
@@ -3,12 +3,12 @@ from django.db.models import OuterRef, Avg, Subquery, Count
 
 
 class DogQuerySet(models.QuerySet):
-    def breed_average_age(self) -> models.QuerySet:
-        sub = self.objects.filter(breed=OuterRef("breed")).values("breed").annotate(avg_age=Avg("age")).values("avg_age")
-        query = self.objects.annotate(avg_age_breed=Subquery(sub))
+    def with_breed_average_age(self) -> models.QuerySet:
+        sub = self.filter(breed=OuterRef("breed")).values("breed").annotate(avg_age=Avg("age")).values("avg_age")
+        query = self.annotate(avg_age_breed=Subquery(sub))
         return query
 
-    def same_breed_count(self):
-        sub = self.objects.filter(breed=OuterRef("breed")).values("breed").annotate(count_dog=Count("id")).values("count_dog")
-        query = self.objects.annotate(count_dog=Subquery(sub))
+    def with_same_breed_count(self):
+        sub = self.filter(breed=OuterRef("breed")).values("breed").annotate(count_dog=Count("id")).values("count_dog")
+        query = self.annotate(count_dog=Subquery(sub))
         return query
